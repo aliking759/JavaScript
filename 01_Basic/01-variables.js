5c22c0952ca4ccc5931d3a40da28285f41b462d1
aliking759 <125295276+aliking759@users.noreply.github.com>
2023-10-21 15:00:34 -0700

    update 1 variables

--- 01_Basic/01-variables.js
+++ 01_Basic/01-variables.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+document.write("hello world");
+
+const userId = 1223355;
+let userEmail = "heroki467@gmail.com";
+var userPassword = "mahi7878"; // ye pahlay waat ma  use hota tha lakin ab es ki
+//jagha let or const ka use keya jata hai
+userCity = "lahore"; //   This is not  using he is very danger
+
+// accountId = 2;
+//   This is not  using he is very danger
+//  es tarha app nay kabhi b code ni lekhna
+userEmail = "aliheroo423@gmail.com";
+userPassword = "nahi7878";
+userCity = "pakistan";
+
+document.write(userId); //es tarha hum code ko apnay browser pr run kr saktay hain
+document.write(userEmail); //es tarha hum code ko apnay browser pr run kr saktay hain
+document.write(userPassword); //es tarha hum code ko apnay browser pr run kr saktay hain
+document.write(userCity); //es tarha hum code ko apnay browser pr run kr saktay hain
+
+// document.write(userEmail, userId, userCity, userPassword);
+console.table(userEmail, userId, userCity, userPassword); //es tarha hum code ko apnay browser kay console pr dakh saktay hain
