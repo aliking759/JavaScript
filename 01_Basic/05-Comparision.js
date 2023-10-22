b6799fe07a0b48d0b448077c91663cc7b5b41665
aliking759 <125295276+aliking759@users.noreply.github.com>
2023-10-22 09:16:42 -0700

    Comparision opratar

---
+++ 01_Basic/05-Comparision.js
@@ -0,0 +1,13 @@
+//```````````````````````````Comparision ```````````````//
+// console.log(2 == 2); // out put ture
+// console.log(2 != 2); // out put false
+
+// console.log("2" > 2); // out put false
+// console.log("2" > 1); // out put ture
+
+console.log(null == 0); //out put false
+console.log(null >= 0); // out put ture
+console.log(null > 0); //out put false
+
+// === this is a very important concept
+console.log("2" === 1); // out put false
