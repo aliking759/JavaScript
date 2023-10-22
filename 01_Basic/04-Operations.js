6403b9c86cf6e45753820d7c43b9a8be4843258d
aliking759 <125295276+aliking759@users.noreply.github.com>
2023-10-22 06:59:02 -0700

    Operations

---
+++ 01_Basic/04-Operations.js
@@ -0,0 +1,30 @@
+// ------------------Operations----------------//
+let a = 10;
+let b = 20;
+console.log(a + b); // out put 30
+
+// -----------------basic operations---------------------//
+// console.log(2 + 2); // out put 4
+// console.log(2 - 2); // out put 0
+// console.log(2 / 2); // out put 1
+//console.log(2 * 2); // out put 4
+//console.log(2 % 2); // out put 0
+// console.log(2 ** 3); // out put 4
+
+let firstname = "ali";
+let lastname = " king";
+let output = firstname + lastname;
+console.log(output);
+//Answer ali king // is tarha say hum 2 string ko b aapas main jor saktay hain
+
+console.log(1 + 2); // out put 3
+console.log("1" + 2); // out put 12
+console.log(1 + "2"); // out put 12
+console.log(1 + 2 + "2"); // out put 32
+
+let StartGame = 99;
+StartGame++;
+console.log(StartGame);
+// out put 100 agar hum kisi kay sath ++ LAGATAY HAIN TOO  us main ik or add kr deya jata hai
+// Link too study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
