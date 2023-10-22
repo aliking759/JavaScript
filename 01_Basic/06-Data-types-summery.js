f262e1d0ce76e2d457ee37d46e7a7f7b7fb7c39d
aliking759 <125295276+aliking759@users.noreply.github.com>
2023-10-22 12:05:57 -0700

    data types summery

---
+++ 01_Basic/06-Data-types-summery.js
@@ -0,0 +1,23 @@
+//`````````````````` Primitive data types ````````````````````
+//primitive data types  7 ka hotay hain
+// 1.String 2. Number 3.Bigint 4.Null 5.symbal 6.boolean 7.Undefined
+
+//```````````````````` Reference data types ```````````````````````
+
+// Reference data types 3 tarha kay hotay hain
+// 1.Array 2.Object 3. Functions
+// 1. Array ye ik array hai
+const heros = ["php hazrat aadam", "hazrat eesa", "hazrat muhammad "];
+
+// 2. Object ye ik object hai
+
+let myobj = {
+  newname: "ali",
+  age: 22,
+};
+// 1. Function ye ik funcyion hai
+
+const myFunction = function () {
+  console.log("hello world");
+};
+console.log(typeof myFunction); // output function
