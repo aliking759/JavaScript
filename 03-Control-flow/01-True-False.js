d0dde6a2f3097594d9ee6d0d010015631be4462b
aliking759 <125295276+aliking759@users.noreply.github.com>
2023-10-23 18:51:15 -0700

    01-true-and-false

---
+++ 03-Control-flow/01-True-False.js
@@ -0,0 +1,15 @@
+const teacher = "30";
+// if (teacher > 30) {
+///// output teacher is not come in the class
+if (teacher == 30) {
+  // output teacher in a class
+  console.log("teacher in a class");
+} else {
+  console.log("teacher is not come in the class");
+}
+
+const score = 100;
+if (score > 90) {
+  const power = "fly";
+  console.log(`user power ${power}`);
+}
