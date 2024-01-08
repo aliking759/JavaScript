2680a72ff400e2d1363602108f2307928b019324
Muhammad Waseem <muhammad.waseem@techliance.com>
2024-01-08 14:10:11 +0500

    update

---
+++ Practice_Questions/index.js
@@ -0,0 +1,15 @@
+//Q1;
+//Print all number
+// let is = 0;
+// for (let is = 0; is <= 100; is++) {
+//   console.log("is=", is);
+// }
+//Q2;
+// this is Even Number
+let is = 0;
+for (let is = 0; is <= 100; is++) {
+  if (is % 2 === 0) {
+    console.log("is=", is);
+  } else {
+  }
+}
